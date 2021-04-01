0b11a0807c62c4171765ec32e24321e3fecc1dfe
Geoffrey Hil <g.hill@sae.edu>
2021-04-01 14:43:08 +1100

    Initial commit from Week 9 in class exercise.

---
+++ Assets/PieceLocationIndicator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceLocationIndicator : MonoBehaviour
+{
+    public Renderer r;
+
+    public Material red, transparent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //r = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        r.material = red;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //r.material.color = Color.grey;
+        r.material = transparent;
+    }
+}
