5f6fa964128df7ecb85dadca45b651b36ac73241
Geoffrey Hil <g.hill@sae.edu>
2021-04-15 13:19:19 +1000

    Added extension to OVRGrabbable

---
+++ Assets/GHPickupTwo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GHPickupTwo : OVRGrabbable
+{
+    Renderer r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
+    {
+        base.GrabBegin(hand, grabPoint);
+        //now do the things I want to do
+        r.material.color = Color.blue;
+    }
+
+ 
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(linearVelocity, angularVelocity);
+        r.material.color = Color.green;
+    }
+}
---
+++ Assets/PieceManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceManager : MonoBehaviour
+{
+    public enum PieceType { Pawn, King };
+    public PieceType myPieceType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(myPieceType == PieceType.King)
+        {
+            //i am a king
+            
+        }
+
+        if(myPieceType == PieceType.Pawn)
+        {
+            //i am a pawn.
+        }
+
+    }
+}
